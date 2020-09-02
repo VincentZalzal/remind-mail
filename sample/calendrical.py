3161021912e21f3d4864ce2455a0451d4d542b55
Vincent Zalzal <vincent.zalzal@gmail.com>
2020-09-01 23:58:52 -0400

    Handle default_time.

--- sample/calendrical.py
+++ sample/calendrical.py
@@ -1,6 +1,7 @@
 import datetime
 
 def get_time(config, time_str):
+    safe_time_str = time_str if time_str is not None else config['default_time']
     aliases = config.get('time_aliases', {})
-    resolved_time_str = aliases.get(time_str, time_str)
+    resolved_time_str = aliases.get(safe_time_str, safe_time_str)
     return datetime.datetime.strptime(resolved_time_str, '%H:%M').time()
--- tests/test_calendrical.py
+++ tests/test_calendrical.py
@@ -14,3 +14,18 @@ def test_get_time():
     assert sample.calendrical.get_time(config, '9:31') == datetime.time(9, 31)
     assert sample.calendrical.get_time(config, 'morning') == datetime.time(8, 12)
     assert sample.calendrical.get_time(config, 'evening') == datetime.time(21, 34)
+
+def test_get_time_default():
+    config = {
+        'default_time': '10:12',
+        'time_aliases': {
+            'morning': '8:12',
+            'evening': '21:34'
+            }
+        }
+    
+    assert sample.calendrical.get_time(config, '9:31') == datetime.time(9, 31)
+    assert sample.calendrical.get_time(config, None) == datetime.time(10, 12)
+
+    config['default_time'] = 'evening'
+    assert sample.calendrical.get_time(config, None) == datetime.time(21, 34)
