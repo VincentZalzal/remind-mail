d15152d0cf9983b227a095116f6f9ea0a091368e
Vincent Zalzal <vincent.zalzal@gmail.com>
2020-09-04 00:56:47 -0400

    Temp initial messages_between

--- sample/calendrical.py
+++ sample/calendrical.py
@@ -5,3 +5,10 @@ def get_time(config, time_str):
     aliases = config.get('time_aliases', {})
     resolved_time_str = aliases.get(safe_time_str, safe_time_str)
     return datetime.datetime.strptime(resolved_time_str, '%H:%M').time()
+
+def messages_between(config, start_datetime, end_datetime):
+    for rule in config['rules']:
+        time = get_time(config, rule.get('time'))
+        #   must add time to the dates in the rule...
+        #start = max(start_datetime, rule.get('start_date', start_datetime))
+        #end = min(end_datetime, rule.get('end_date', end_datetime))
