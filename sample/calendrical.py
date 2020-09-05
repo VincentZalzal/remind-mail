15f86d6d8a4c033a1ff3a203341c9eadd28194d9
Vincent Zalzal <vincent.zalzal@gmail.com>
2020-09-05 00:21:06 -0400

    Implement messages_between. Not fully implemented nor tested. Start testing.

--- sample/calendrical.py
+++ sample/calendrical.py
@@ -1,14 +1,41 @@
-import datetime
+from datetime import datetime
 
 def get_time(config, time_str):
     safe_time_str = time_str if time_str is not None else config['default_time']
     aliases = config.get('time_aliases', {})
     resolved_time_str = aliases.get(safe_time_str, safe_time_str)
-    return datetime.datetime.strptime(resolved_time_str, '%H:%M').time()
+    return datetime.strptime(resolved_time_str, '%H:%M').time()
+
+def get_datetime(rule, date_name, time):
+    date_str = rule.get(date_name)
+    if date_str is None:
+        return None
+    date = datetime.strptime(date_str, '%Y-%m-%d').date()
+    return datetime.combine(date, time)
+
+def find_next_date(rule, start):
+    next_date = start # TODO implement (switch on rule type)
+    return next_date
+
+def message_to_add(rule, start_datetime, end_datetime):
+    time = get_time(config, rule.get('time'))
+    rule_start = get_datetime(rule, 'start_date', time)
+    rule_end = get_datetime(rule, 'end_date', time)
+    start = start_datetime if rule_start is None else max(start_datetime, rule_start)
+    end = end_datetime if rule_end is None else min(end_datetime, rule_end)
+
+    next_date = find_next_date(rule, start)
+    if next_date <= end:
+        return {key: rule[key] for key in ['subject', 'body']}
+    else:
+        return None
 
 def messages_between(config, start_datetime, end_datetime):
+    messages = []
+
     for rule in config['rules']:
-        time = get_time(config, rule.get('time'))
-        #   must add time to the dates in the rule...
-        #start = max(start_datetime, rule.get('start_date', start_datetime))
-        #end = min(end_datetime, rule.get('end_date', end_datetime))
+        msg = message_to_add(rule, start_datetime, end_datetime)
+        if msg is not None:
+            messages.append(msg)
+
+    return messages
--- tests/test_calendrical.py
+++ tests/test_calendrical.py
@@ -1,31 +1,41 @@
 from .context import sample
 import sample.calendrical
-import datetime
+from datetime import datetime, time
 
 def test_get_time():
     config = {
         'time_aliases': {
             'morning': '8:12',
-            'evening': '21:34'
-            }
+            'evening': '21:34',
+            },
         }
 
-    assert sample.calendrical.get_time({}, '9:31') == datetime.time(9, 31)
-    assert sample.calendrical.get_time(config, '9:31') == datetime.time(9, 31)
-    assert sample.calendrical.get_time(config, 'morning') == datetime.time(8, 12)
-    assert sample.calendrical.get_time(config, 'evening') == datetime.time(21, 34)
+    assert sample.calendrical.get_time({}, '9:31') == time(9, 31)
+    assert sample.calendrical.get_time(config, '9:31') == time(9, 31)
+    assert sample.calendrical.get_time(config, 'morning') == time(8, 12)
+    assert sample.calendrical.get_time(config, 'evening') == time(21, 34)
 
 def test_get_time_default():
     config = {
         'default_time': '10:12',
         'time_aliases': {
             'morning': '8:12',
-            'evening': '21:34'
-            }
+            'evening': '21:34',
+            },
         }
     
-    assert sample.calendrical.get_time(config, '9:31') == datetime.time(9, 31)
-    assert sample.calendrical.get_time(config, None) == datetime.time(10, 12)
+    assert sample.calendrical.get_time(config, '9:31') == time(9, 31)
+    assert sample.calendrical.get_time(config, None) == time(10, 12)
 
     config['default_time'] = 'evening'
-    assert sample.calendrical.get_time(config, None) == datetime.time(21, 34)
+    assert sample.calendrical.get_time(config, None) == time(21, 34)
+
+def test_get_datetime():
+    rule = {
+        'start_date': '2020-09-05',
+        'end_date': '2021-10-10',
+        }
+
+    assert sample.calendrical.get_datetime(rule, 'start_date', time(10, 12)) == datetime(2020, 9, 5, 10, 12)
+    assert sample.calendrical.get_datetime(rule, 'end_date', time(11, 12)) == datetime(2021, 10, 10, 11, 12)
+    assert sample.calendrical.get_datetime(rule, 'unknown', time(12, 12)) is None
