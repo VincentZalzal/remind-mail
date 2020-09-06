1758a3d7d53a0dc5bdebca040b72f139722c50b2
Vincent Zalzal <vincent.zalzal@gmail.com>
2020-09-06 00:38:51 -0400

    Refactoring

--- sample/calendrical.py
+++ sample/calendrical.py
@@ -6,26 +6,27 @@ def get_time(config, time_str):
     resolved_time_str = aliases.get(safe_time_str, safe_time_str)
     return datetime.strptime(resolved_time_str, '%H:%M').time()
 
-def get_datetime(rule, date_name, time):
-    date_str = rule.get(date_name)
-    if date_str is None:
+def get_datetime(date_str, time):
+    if date_str is not None:
+        date = datetime.strptime(date_str, '%Y-%m-%d').date()
+        return datetime.combine(date, time)
+    else:
         return None
-    date = datetime.strptime(date_str, '%Y-%m-%d').date()
-    return datetime.combine(date, time)
 
-def find_next_date(rule, start):
-    next_date = start # TODO implement (switch on rule type)
-    return next_date
+def find_next_datetime(when_str, rule_start_datetime, rule_time, min_datetime):
+    # rule_start_datetime may be None
+    next_datetime = min_datetime # TODO implement (switch on rule type)
+    return next_datetime
 
 def message_to_add(rule, start_datetime, end_datetime):
-    time = get_time(config, rule.get('time'))
-    rule_start = get_datetime(rule, 'start_date', time)
-    rule_end = get_datetime(rule, 'end_date', time)
-    start = start_datetime if rule_start is None else max(start_datetime, rule_start)
-    end = end_datetime if rule_end is None else min(end_datetime, rule_end)
+    rule_time = get_time(config, rule.get('time'))
+    rule_start_datetime = get_datetime(rule.get('start_date'), rule_time)
+    rule_end_datetime = get_datetime(rule.get('end_date'), rule_time)
+    min_datetime = start_datetime if rule_start_datetime is None else max(start_datetime, rule_start_datetime)
+    max_datetime = end_datetime if rule_end_datetime is None else min(end_datetime, rule_end_datetime)
 
-    next_date = find_next_date(rule, start)
-    if next_date <= end:
+    next_datetime = find_next_datetime(rule['every'], rule_start_datetime, rule_time, min_datetime)
+    if next_datetime <= max_datetime:
         return {key: rule[key] for key in ['subject', 'body']}
     else:
         return None
--- tests/test_calendrical.py
+++ tests/test_calendrical.py
@@ -31,11 +31,6 @@ def test_get_time_default():
     assert sample.calendrical.get_time(config, None) == time(21, 34)
 
 def test_get_datetime():
-    rule = {
-        'start_date': '2020-09-05',
-        'end_date': '2021-10-10',
-        }
-
-    assert sample.calendrical.get_datetime(rule, 'start_date', time(10, 12)) == datetime(2020, 9, 5, 10, 12)
-    assert sample.calendrical.get_datetime(rule, 'end_date', time(11, 12)) == datetime(2021, 10, 10, 11, 12)
-    assert sample.calendrical.get_datetime(rule, 'unknown', time(12, 12)) is None
+    assert sample.calendrical.get_datetime('2020-09-05', time(10, 12)) == datetime(2020, 9, 5, 10, 12)
+    assert sample.calendrical.get_datetime('2021-10-10', time(11, 12)) == datetime(2021, 10, 10, 11, 12)
+    assert sample.calendrical.get_datetime(None, time(12, 12)) is None
