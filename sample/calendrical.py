7b20db1b2bd4cb347166b110d4a7a3386b336262
Vincent Zalzal <vincent.zalzal@gmail.com>
2020-09-07 12:53:54 -0400

    Refactoring

--- sample/calendrical.py
+++ sample/calendrical.py
@@ -16,33 +16,33 @@ def get_datetime(date_str, time):
     else:
         return None
 
-def find_first_datetime(rule_time, min_datetime):
-    first_date = min_datetime.date()
+def next_time(min_datetime, rule_time):
+    '''Return first datetime >= min_datetime where time == rule_time'''
+    date = min_datetime.date()
     if min_datetime.time() > rule_time:
-        first_date = first_date + timedelta(days=1)
-    return datetime.combine(first_date, rule_time)
+        date = date + timedelta(days=1)
+    return datetime.combine(date, rule_time)
 
-def find_next_datetime_weekly(day_idx, first_datetime):
-    days_to_next_weekday = (day_idx - first_datetime.weekday()) % 7
-    return first_datetime + timedelta(days=days_to_next_weekday)
+def next_datetime_weekly(min_datetime, day_idx):
+    days_to_next_weekday = (day_idx - min_datetime.weekday()) % 7
+    return min_datetime + timedelta(days=days_to_next_weekday)
 
-def find_next_datetime_yearly(when_str, first_datetime):
+def next_datetime_yearly(min_datetime, when_str):
     date = datetime.strptime(when_str, '%m-%d')
-    next_datetime = first_datetime.replace(month=date.month, day=date.day)
-    if next_datetime < first_datetime:
+    next_datetime = min_datetime.replace(month=date.month, day=date.day)
+    if next_datetime < min_datetime:
         # Add a year safely. In theory, not needed, as strptime('%m-%d') does not allow 02-29
         if next_datetime.month == 2 and next_datetime.day == 29:
             next_datetime = next_datetime.replace(month=3, day=1)
         next_datetime = next_datetime.replace(year = next_datetime.year + 1)
     return next_datetime
 
-def find_next_datetime(when_str, rule_start_datetime, rule_time, min_datetime):
-    # rule_start_datetime may be None
-    first_datetime = find_first_datetime(rule_time, min_datetime)
-    if when_str in _weekdays:
-        return find_next_datetime_weekly(_weekdays.index(when_str), first_datetime)
+def find_next_datetime(min_datetime, when_str, rule_start_datetime = None):
+    '''Parse "every" string, then defer to appropriate calendrical method'''
+    if when_str in _weekdays: # TODO make case-insensitive
+        return next_datetime_weekly(min_datetime, _weekdays.index(when_str))
     elif re.match(r"\d?\d-\d?\d", when_str) is not None:
-        return find_next_datetime_yearly(when_str, first_datetime)
+        return next_datetime_yearly(min_datetime, when_str)
     else:
         return min_datetime # TODO implement (switch on rule type: 'day N')
 
@@ -51,9 +51,10 @@ def message_to_add(rule, start_datetime, end_datetime):
     rule_start_datetime = get_datetime(rule.get('start_date'), rule_time)
     rule_end_datetime = get_datetime(rule.get('end_date'), rule_time)
     min_datetime = start_datetime if rule_start_datetime is None else max(start_datetime, rule_start_datetime)
+    min_datetime = next_time(min_datetime, rule_time)
     max_datetime = end_datetime if rule_end_datetime is None else min(end_datetime, rule_end_datetime)
 
-    next_datetime = find_next_datetime(rule['every'], rule_start_datetime, rule_time, min_datetime)
+    next_datetime = find_next_datetime(min_datetime, rule['every'], rule_start_datetime)
     if next_datetime <= max_datetime:
         return {key: rule[key] for key in ['subject', 'body']}
     else:
--- tests/test_calendrical.py
+++ tests/test_calendrical.py
@@ -35,28 +35,28 @@ def test_get_datetime():
     assert sample.calendrical.get_datetime('2021-10-10', time(11, 12)) == datetime(2021, 10, 10, 11, 12)
     assert sample.calendrical.get_datetime(None, time(12, 12)) is None
 
-def test_find_first_datetime():
+def test_next_time():
     min_datetime = datetime(2020, 9, 17, 8, 15)
-    assert sample.calendrical.find_first_datetime(time(8, 15), min_datetime) == min_datetime
-    assert sample.calendrical.find_first_datetime(time(7, 20), min_datetime) == datetime(2020, 9, 18, 7, 20)
-    assert sample.calendrical.find_first_datetime(time(9, 10), min_datetime) == datetime(2020, 9, 17, 9, 10)
+    assert sample.calendrical.next_time(min_datetime, time(8, 15)) == min_datetime
+    assert sample.calendrical.next_time(min_datetime, time(7, 20)) == datetime(2020, 9, 18, 7, 20)
+    assert sample.calendrical.next_time(min_datetime, time(9, 10)) == datetime(2020, 9, 17, 9, 10)
 
-def test_find_next_datetime_weekly():
+def test_next_datetime_weekly():
     thursday = datetime(2020, 9, 17, 8, 15) # idx = 3
-    assert sample.calendrical.find_next_datetime_weekly(3, thursday) == thursday
-    assert sample.calendrical.find_next_datetime_weekly(4, thursday) == thursday + timedelta(days=1)
-    assert sample.calendrical.find_next_datetime_weekly(2, thursday) == thursday + timedelta(days=6)
+    assert sample.calendrical.next_datetime_weekly(thursday, 3) == thursday
+    assert sample.calendrical.next_datetime_weekly(thursday, 4) == thursday + timedelta(days=1)
+    assert sample.calendrical.next_datetime_weekly(thursday, 2) == thursday + timedelta(days=6)
 
-def test_find_next_datetime_yearly():
+def test_next_datetime_yearly():
     first_datetime = datetime(2020, 9, 17, 8, 15)
-    assert sample.calendrical.find_next_datetime_yearly('9-17', first_datetime) == first_datetime
-    assert sample.calendrical.find_next_datetime_yearly('10-17', first_datetime) == first_datetime.replace(month=10)
-    assert sample.calendrical.find_next_datetime_yearly('9-18', first_datetime) == first_datetime.replace(day=18)
-    assert sample.calendrical.find_next_datetime_yearly('08-17', first_datetime) == first_datetime.replace(year=2021, month=8)
-    assert sample.calendrical.find_next_datetime_yearly('09-16', first_datetime) == first_datetime.replace(year=2021, day=16)
+    assert sample.calendrical.next_datetime_yearly(first_datetime, '9-17') == first_datetime
+    assert sample.calendrical.next_datetime_yearly(first_datetime, '10-17') == first_datetime.replace(month=10)
+    assert sample.calendrical.next_datetime_yearly(first_datetime, '9-18') == first_datetime.replace(day=18)
+    assert sample.calendrical.next_datetime_yearly(first_datetime, '08-17') == first_datetime.replace(year=2021, month=8)
+    assert sample.calendrical.next_datetime_yearly(first_datetime, '09-16') == first_datetime.replace(year=2021, day=16)
 
 def test_find_next_datetime():
     thursday = datetime(2020, 9, 17, 8, 15)
-    assert sample.calendrical.find_next_datetime('Wednesday', None, thursday.time(), thursday) == thursday + timedelta(days=6)
-    assert sample.calendrical.find_next_datetime('08-11', None, thursday.time(), thursday) == datetime(2021, 8, 11, 8, 15)
+    assert sample.calendrical.find_next_datetime(thursday, 'Wednesday') == thursday + timedelta(days=6)
+    assert sample.calendrical.find_next_datetime(thursday, '08-11') == datetime(2021, 8, 11, 8, 15)
     # TODO more tests here
