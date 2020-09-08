37b3e501848944d7fdf6827b7d3a7e0ddad8e743
Vincent Zalzal <vincent.zalzal@gmail.com>
2020-09-07 22:58:08 -0400

    Refactor to remove time almost everywhere and handle dates only

--- sample/calendrical.py
+++ sample/calendrical.py
@@ -7,66 +7,55 @@ def get_time(config, time_str):
     resolved_time_str = aliases.get(safe_time_str, safe_time_str)
     return datetime.strptime(resolved_time_str, '%H:%M').time()
 
-def get_datetime(date_str, time):
-    if date_str is not None:
-        date = datetime.strptime(date_str, '%Y-%m-%d').date()
-        return datetime.combine(date, time)
-    else:
-        return None
+def parse_date(date_str):
+    return datetime.strptime(date_str, '%Y-%m-%d').date() if date_str is not None else None
 
-def next_time(min_datetime, rule_time):
-    '''Return first datetime >= min_datetime where time == rule_time'''
-    date = min_datetime.date()
-    if min_datetime.time() > rule_time:
-        date = date + timedelta(days=1)
-    return datetime.combine(date, rule_time)
-
-def next_datetime_daily(min_datetime, freq = 1, start_datetime = None):
+def next_date_daily(min_date, freq = 1, start_date = None):
     assert freq >= 1
-    assert start_datetime is None or min_datetime.time() == start_datetime.time()
+    assert start_date is None or start_date <= min_date
     raise Exception('Not implemented yet') # TODO implement
 
-def next_datetime_weekly(min_datetime, days, freq = 1, start_datetime = None):
+def next_date_weekly(min_date, days, freq = 1, start_date = None):
     assert len(days) >= 1
     assert sorted(days) == days
     assert freq >= 1
-    assert start_datetime is None or min_datetime.time() == start_datetime.time()
+    assert start_date is None or start_date <= min_date
     
-    days_to_next_weekday = min( (day - min_datetime.weekday()) % 7 for day in days)
-    next_datetime = min_datetime + timedelta(days=days_to_next_weekday)
+    weekday = min_date.weekday()
+    days_to_next_weekday = min( (day - weekday) % 7 for day in days)
+    next_date = min_date + timedelta(days=days_to_next_weekday)
     
     if freq > 1:
         assert len(days) == 1
-        assert start_datetime is not None
-        assert start_datetime.weekday() == days[0]
-        assert start_datetime <= min_datetime and min_datetime <= next_datetime
-        assert next_datetime.weekday() == start_datetime.weekday()
-        num_elapsed_weeks = (next_datetime - start_datetime).days // 7
+        assert start_date is not None
+        assert start_date.weekday() == days[0]
+        assert start_date <= min_date and min_date <= next_date
+        assert next_date.weekday() == start_date.weekday()
+        num_elapsed_weeks = (next_date - start_date).days // 7
         weeks_to_add = -num_elapsed_weeks % freq
-        next_datetime = next_datetime + timedelta(days = weeks_to_add*7)
+        next_date = next_date + timedelta(days = weeks_to_add*7)
     
-    return next_datetime
+    return next_date
 
-def next_datetime_monthly_on_day(min_datetime, day, freq = 1, start_datetime = None):
+def next_date_monthly_on_day(min_date, day, freq = 1, start_date = None):
     assert freq >= 1
-    assert start_datetime is None or min_datetime.time() == start_datetime.time()
+    assert start_date is None or start_date <= min_date
     raise Exception('Not implemented yet') # TODO implement
 
-def next_datetime_monthly_on_week(min_datetime, weekday, weeknum, freq = 1, start_datetime = None):
+def next_date_monthly_on_week(min_date, weekday, weeknum, freq = 1, start_date = None):
     assert freq >= 1
-    assert start_datetime is None or min_datetime.time() == start_datetime.time()
+    assert start_date is None or start_date <= min_date
     raise Exception('Not implemented yet') # TODO implement
 
-def next_datetime_yearly(min_datetime, freq, start_datetime):
+def next_date_yearly(min_date, freq, start_date):
     assert freq >= 1
-    assert not (start_datetime.month == 2 and start_datetime.day == 29)
-    assert min_datetime.time() == start_datetime.time()
+    assert not (start_date.month == 2 and start_date.day == 29)
 
-    # how many multiple of freq years must I add to start_datetime so that it is >= min_datetime?
-    next_datetime = start_datetime
-    while next_datetime < min_datetime:
-        next_datetime = next_datetime.replace(year=next_datetime.year + freq)
-    return next_datetime
+    # how many multiple of freq years must I add to start_date so that it is >= min_date?
+    next_date = start_date
+    while next_date < min_date:
+        next_date = next_date.replace(year=next_date.year + freq)
+    return next_date
 
 class ReMatcher:
     def __init__(self):
@@ -93,53 +82,63 @@ def parse_when(when_str):
     matcher = ReMatcher()
     if matcher.matches(_regexes.daily, when_str_lc):
         freq = int(matcher.match.group(1)) if matcher.match.group(1) is not None else 1
-        return (next_datetime_daily, {'freq': freq})
+        return (next_date_daily, {'freq': freq})
     elif when_str_lc in _weekdays:
-        return (next_datetime_weekly, {'days': [_weekdays.index(when_str_lc)]})
+        return (next_date_weekly, {'days': [_weekdays.index(when_str_lc)]})
     elif when_str_lc == 'weekday':
-        return (next_datetime_weekly, {'days': [0,1,2,3,4]})
+        return (next_date_weekly, {'days': [0,1,2,3,4]})
     elif matcher.matches(_regexes.weekly, when_str_lc):
         freq = int(matcher.match.group(1)) if matcher.match.group(1) is not None else 1
         day = _weekdays.index(matcher.match.group(2))
-        return (next_datetime_weekly, {'freq': freq, 'days': [day]})
+        return (next_date_weekly, {'freq': freq, 'days': [day]})
     elif matcher.matches(_regexes.monthly_on_day, when_str_lc):
         freq = int(matcher.match.group(1)) if matcher.match.group(1) is not None else 1
         day = int(matcher.match.group(2))
-        return (next_datetime_monthly_on_day, {'freq': freq, 'day': day})
+        return (next_date_monthly_on_day, {'freq': freq, 'day': day})
     elif matcher.matches(_regexes.monthly_on_week, when_str_lc):
         freq = int(matcher.match.group(1)) if matcher.match.group(1) is not None else 1
         weekday = _weekdays.index(matcher.match.group(2))
         weeknum = int(matcher.match.group(3))
-        return (next_datetime_monthly_on_week, {'freq': freq, 'weekday': weekday, 'weeknum': weeknum})
+        return (next_date_monthly_on_week, {'freq': freq, 'weekday': weekday, 'weeknum': weeknum})
     elif matcher.matches(_regexes.yearly, when_str_lc):
         freq = int(matcher.match.group(1)) if matcher.match.group(1) is not None else 1
-        return (next_datetime_yearly, {'freq': freq})
+        return (next_date_yearly, {'freq': freq})
     else:
         raise Exception(f"Unable to parse '{when_str}'") # TODO implement (switch on rule type: 'day N')
 
-def find_next_datetime(min_datetime, when_str, rule_start_datetime = None):
+def find_next_date(min_date, when_str, rule_start_date = None):
     '''Parse "every" string, then defer to appropriate calendrical method'''
-    assert rule_start_datetime is None or min_datetime.time() == rule_start_datetime.time()
+    assert rule_start_date is None or rule_start_date <= min_date
     func, args = parse_when(when_str)
-    return func(min_datetime, start_datetime=rule_start_datetime, **args)
+    return func(min_date, start_date=rule_start_date, **args)
 
-def add_next_datetime(rule, start_datetime):
-    rule_time = rule['_rule_time']
-    rule_start_datetime = get_datetime(rule.get('start_date'), rule_time)
-    min_datetime = start_datetime if rule_start_datetime is None else max(start_datetime, rule_start_datetime)
-    min_datetime = next_time(min_datetime, rule_time)
-    rule['_next_datetime'] = find_next_datetime(min_datetime, rule['every'], rule_start_datetime)
+def add_next_date(rule, start_datetime):
+    min_date = start_datetime.date()
+    if start_datetime.time() > rule['_rule_time']:
+        min_date = min_date + timedelta(days=1)
+    rule_start_date = parse_date(rule.get('start_date'))
+    if rule_start_date is not None and rule_start_date > min_date:
+        min_date = rule_start_date
+    
+    next_date = find_next_date(min_date, rule['every'], rule_start_date)
 
-def add_next_dates(config, start_datetime):
+    rule_end_date = parse_date(rule.get('end_date'))
+    if rule_end_date is not None and rule_end_date < next_date:
+        next_date = None
+
+    rule['_next_date'] = next_date
+
+def add_next_dates_and_times(config, start_datetime):
     for rule in config['rules']:
         rule['_rule_time'] = get_time(config, rule.get('time'))
-        add_next_datetime(rule, start_datetime)
+        add_next_date(rule, start_datetime)
 
 def is_before(rule, end_datetime):
-    rule_end_datetime = get_datetime(rule.get('end_date'), rule['_rule_time'])
-    max_datetime = end_datetime if rule_end_datetime is None else min(end_datetime, rule_end_datetime)
-    next_datetime = rule['_next_datetime']
-    return next_datetime <= max_datetime
+    next_date = rule['_next_date']
+    if next_date is None:
+        return False
+    next_datetime = datetime.combine(next_date, rule['_rule_time'])
+    return next_datetime <= end_datetime
 
 def messages_before(config, end_datetime):
     messages = []
--- tests/test_calendrical.py
+++ tests/test_calendrical.py
@@ -1,6 +1,6 @@
 from .context import sample
 import sample.calendrical
-from datetime import datetime, time, timedelta
+from datetime import datetime, date, time, timedelta
 
 def test_get_time():
     config = {
@@ -30,70 +30,64 @@ def test_get_time_default():
     config['default_time'] = 'evening'
     assert sample.calendrical.get_time(config, None) == time(21, 34)
 
-def test_get_datetime():
-    assert sample.calendrical.get_datetime('2020-09-05', time(10, 12)) == datetime(2020, 9, 5, 10, 12)
-    assert sample.calendrical.get_datetime('2021-10-10', time(11, 12)) == datetime(2021, 10, 10, 11, 12)
-    assert sample.calendrical.get_datetime(None, time(12, 12)) is None
+def test_parse_date():
+    assert sample.calendrical.parse_date('2020-09-05') == date(2020, 9, 5)
+    assert sample.calendrical.parse_date('2021-10-10') == date(2021, 10, 10)
+    assert sample.calendrical.parse_date(None) is None
 
-def test_next_time():
-    min_datetime = datetime(2020, 9, 17, 8, 15)
-    assert sample.calendrical.next_time(min_datetime, time(8, 15)) == min_datetime
-    assert sample.calendrical.next_time(min_datetime, time(7, 20)) == datetime(2020, 9, 18, 7, 20)
-    assert sample.calendrical.next_time(min_datetime, time(9, 10)) == datetime(2020, 9, 17, 9, 10)
+def test_next_date_weekly():
+    thursday = date(2020, 9, 17) # idx = 3
+    assert sample.calendrical.next_date_weekly(thursday, [3]) == thursday
+    assert sample.calendrical.next_date_weekly(thursday, [4]) == thursday + timedelta(days=1)
+    assert sample.calendrical.next_date_weekly(thursday, [2]) == thursday + timedelta(days=6)
 
-def test_next_datetime_weekly():
-    thursday = datetime(2020, 9, 17, 8, 15) # idx = 3
-    assert sample.calendrical.next_datetime_weekly(thursday, [3]) == thursday
-    assert sample.calendrical.next_datetime_weekly(thursday, [4]) == thursday + timedelta(days=1)
-    assert sample.calendrical.next_datetime_weekly(thursday, [2]) == thursday + timedelta(days=6)
+    assert sample.calendrical.next_date_weekly(thursday, [3, 4]) == thursday
+    assert sample.calendrical.next_date_weekly(thursday, [1, 3]) == thursday
 
-    assert sample.calendrical.next_datetime_weekly(thursday, [3, 4]) == thursday
-    assert sample.calendrical.next_datetime_weekly(thursday, [1, 3]) == thursday
+    assert sample.calendrical.next_date_weekly(thursday, [4, 5]) == thursday + timedelta(days=1)
+    assert sample.calendrical.next_date_weekly(thursday, [1, 4]) == thursday + timedelta(days=1)
+    assert sample.calendrical.next_date_weekly(thursday, [1, 2]) == thursday + timedelta(days=5)
 
-    assert sample.calendrical.next_datetime_weekly(thursday, [4, 5]) == thursday + timedelta(days=1)
-    assert sample.calendrical.next_datetime_weekly(thursday, [1, 4]) == thursday + timedelta(days=1)
-    assert sample.calendrical.next_datetime_weekly(thursday, [1, 2]) == thursday + timedelta(days=5)
+    assert sample.calendrical.next_date_weekly(thursday, [3], freq=3, start_date=thursday) == thursday
+    assert sample.calendrical.next_date_weekly(thursday, [3], freq=3, start_date=thursday - timedelta(days=3*7)) == thursday
+    assert sample.calendrical.next_date_weekly(thursday, [3], freq=3, start_date=thursday - timedelta(days=2*7)) == thursday + timedelta(days=1*7)
+    assert sample.calendrical.next_date_weekly(thursday, [3], freq=3, start_date=thursday - timedelta(days=4*7)) == thursday + timedelta(days=2*7)
 
-    assert sample.calendrical.next_datetime_weekly(thursday, [3], freq=3, start_datetime=thursday) == thursday
-    assert sample.calendrical.next_datetime_weekly(thursday, [3], freq=3, start_datetime=thursday - timedelta(days=3*7)) == thursday
-    assert sample.calendrical.next_datetime_weekly(thursday, [3], freq=3, start_datetime=thursday - timedelta(days=2*7)) == thursday + timedelta(days=1*7)
-    assert sample.calendrical.next_datetime_weekly(thursday, [3], freq=3, start_datetime=thursday - timedelta(days=4*7)) == thursday + timedelta(days=2*7)
+    assert sample.calendrical.next_date_weekly(thursday - timedelta(days=5), [3], freq=3, start_date=thursday - timedelta(days=4*7)) == thursday + timedelta(days=2*7)
 
-    assert sample.calendrical.next_datetime_weekly(thursday - timedelta(days=5), [3], freq=3, start_datetime=thursday - timedelta(days=4*7)) == thursday + timedelta(days=2*7)
+def test_next_date_yearly():
+    rule_date = date(2020, 9, 17)
+    assert sample.calendrical.next_date_yearly(rule_date, 1, rule_date) == rule_date
+    assert sample.calendrical.next_date_yearly(date(2020, 6, 20), 1, rule_date) == rule_date
+    assert sample.calendrical.next_date_yearly(date(2021, 6, 20), 1, rule_date) == rule_date.replace(year=2021)
+    assert sample.calendrical.next_date_yearly(date(2021, 10, 5), 1, rule_date) == rule_date.replace(year=2022)
 
-def test_next_datetime_yearly():
-    rule_datetime = datetime(2020, 9, 17, 8, 15)
-    assert sample.calendrical.next_datetime_yearly(rule_datetime, 1, rule_datetime) == rule_datetime
-    assert sample.calendrical.next_datetime_yearly(datetime(2020, 6, 20, 8, 15), 1, rule_datetime) == rule_datetime
-    assert sample.calendrical.next_datetime_yearly(datetime(2021, 6, 20, 8, 15), 1, rule_datetime) == rule_datetime.replace(year=2021)
-    assert sample.calendrical.next_datetime_yearly(datetime(2021, 10, 5, 8, 15), 1, rule_datetime) == rule_datetime.replace(year=2022)
-
-    assert sample.calendrical.next_datetime_yearly(rule_datetime, 3, rule_datetime) == rule_datetime
-    assert sample.calendrical.next_datetime_yearly(datetime(2020, 6, 20, 8, 15), 3, rule_datetime) == rule_datetime
-    assert sample.calendrical.next_datetime_yearly(datetime(2021, 6, 20, 8, 15), 3, rule_datetime) == rule_datetime.replace(year=2023)
-    assert sample.calendrical.next_datetime_yearly(datetime(2021, 10, 5, 8, 15), 3, rule_datetime) == rule_datetime.replace(year=2023)
-    assert sample.calendrical.next_datetime_yearly(rule_datetime.replace(year=2023), 3, rule_datetime) == rule_datetime.replace(year=2023)
+    assert sample.calendrical.next_date_yearly(rule_date, 3, rule_date) == rule_date
+    assert sample.calendrical.next_date_yearly(date(2020, 6, 20), 3, rule_date) == rule_date
+    assert sample.calendrical.next_date_yearly(date(2021, 6, 20), 3, rule_date) == rule_date.replace(year=2023)
+    assert sample.calendrical.next_date_yearly(date(2021, 10, 5), 3, rule_date) == rule_date.replace(year=2023)
+    assert sample.calendrical.next_date_yearly(rule_date.replace(year=2023), 3, rule_date) == rule_date.replace(year=2023)
 
 def test_parse_when():
-    assert sample.calendrical.parse_when('day') == (sample.calendrical.next_datetime_daily, {'freq': 1})
-    assert sample.calendrical.parse_when('12 days') == (sample.calendrical.next_datetime_daily, {'freq': 12})
-    assert sample.calendrical.parse_when('tuesday') == (sample.calendrical.next_datetime_weekly, {'days': [1]})
-    assert sample.calendrical.parse_when('weekday') == (sample.calendrical.next_datetime_weekly, {'days': [0,1,2,3,4]})
-    assert sample.calendrical.parse_when('week on friday') == (sample.calendrical.next_datetime_weekly, {'freq': 1, 'days': [4]})
-    assert sample.calendrical.parse_when('3 weeks on friday') == (sample.calendrical.next_datetime_weekly, {'freq': 3, 'days': [4]})
-    assert sample.calendrical.parse_when('month on day 3') == (sample.calendrical.next_datetime_monthly_on_day, {'freq': 1, 'day': 3})
-    assert sample.calendrical.parse_when('2 months on day 13') == (sample.calendrical.next_datetime_monthly_on_day, {'freq': 2, 'day': 13})
-    assert sample.calendrical.parse_when('3 months on day -1') == (sample.calendrical.next_datetime_monthly_on_day, {'freq': 3, 'day': -1})
-    assert sample.calendrical.parse_when('month on friday 1') == (sample.calendrical.next_datetime_monthly_on_week, {'freq': 1, 'weeknum': 1, 'weekday': 4})
-    assert sample.calendrical.parse_when('2 months on tuesday 2') == (sample.calendrical.next_datetime_monthly_on_week, {'freq': 2, 'weeknum': 2, 'weekday': 1})
-    assert sample.calendrical.parse_when('3 months on monday -1') == (sample.calendrical.next_datetime_monthly_on_week, {'freq': 3, 'weeknum': -1, 'weekday': 0})
-    assert sample.calendrical.parse_when('year') == (sample.calendrical.next_datetime_yearly, {'freq': 1})
-    assert sample.calendrical.parse_when('12 years') == (sample.calendrical.next_datetime_yearly, {'freq': 12})
+    assert sample.calendrical.parse_when('day') == (sample.calendrical.next_date_daily, {'freq': 1})
+    assert sample.calendrical.parse_when('12 days') == (sample.calendrical.next_date_daily, {'freq': 12})
+    assert sample.calendrical.parse_when('tuesday') == (sample.calendrical.next_date_weekly, {'days': [1]})
+    assert sample.calendrical.parse_when('weekday') == (sample.calendrical.next_date_weekly, {'days': [0,1,2,3,4]})
+    assert sample.calendrical.parse_when('week on friday') == (sample.calendrical.next_date_weekly, {'freq': 1, 'days': [4]})
+    assert sample.calendrical.parse_when('3 weeks on friday') == (sample.calendrical.next_date_weekly, {'freq': 3, 'days': [4]})
+    assert sample.calendrical.parse_when('month on day 3') == (sample.calendrical.next_date_monthly_on_day, {'freq': 1, 'day': 3})
+    assert sample.calendrical.parse_when('2 months on day 13') == (sample.calendrical.next_date_monthly_on_day, {'freq': 2, 'day': 13})
+    assert sample.calendrical.parse_when('3 months on day -1') == (sample.calendrical.next_date_monthly_on_day, {'freq': 3, 'day': -1})
+    assert sample.calendrical.parse_when('month on friday 1') == (sample.calendrical.next_date_monthly_on_week, {'freq': 1, 'weeknum': 1, 'weekday': 4})
+    assert sample.calendrical.parse_when('2 months on tuesday 2') == (sample.calendrical.next_date_monthly_on_week, {'freq': 2, 'weeknum': 2, 'weekday': 1})
+    assert sample.calendrical.parse_when('3 months on monday -1') == (sample.calendrical.next_date_monthly_on_week, {'freq': 3, 'weeknum': -1, 'weekday': 0})
+    assert sample.calendrical.parse_when('year') == (sample.calendrical.next_date_yearly, {'freq': 1})
+    assert sample.calendrical.parse_when('12 years') == (sample.calendrical.next_date_yearly, {'freq': 12})
 
-def test_find_next_datetime():
-    thursday = datetime(2020, 9, 17, 8, 15)
-    assert sample.calendrical.find_next_datetime(thursday, 'Wednesday') == thursday + timedelta(days=6)
-    assert sample.calendrical.find_next_datetime(thursday + timedelta(days=2), 'weekday') == thursday + timedelta(days=4)
-    assert sample.calendrical.find_next_datetime(thursday, '3 weeks on friday', datetime(2020, 9, 11, 8, 15)) == datetime(2020, 10, 2, 8, 15)
-    assert sample.calendrical.find_next_datetime(thursday, '3 years', datetime(2020, 8, 11, 8, 15)) == datetime(2023, 8, 11, 8, 15)
+def test_find_next_date():
+    thursday = date(2020, 9, 17)
+    assert sample.calendrical.find_next_date(thursday, 'Wednesday') == thursday + timedelta(days=6)
+    assert sample.calendrical.find_next_date(thursday + timedelta(days=2), 'weekday') == thursday + timedelta(days=4)
+    assert sample.calendrical.find_next_date(thursday, '3 weeks on friday', date(2020, 9, 11)) == date(2020, 10, 2)
+    assert sample.calendrical.find_next_date(thursday, '3 years', date(2020, 8, 11)) == date(2023, 8, 11)
     # TODO more tests here
